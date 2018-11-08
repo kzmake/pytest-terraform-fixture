4b59b3c790a1b7990559dc29bbfec8f5a9b2344b
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-11-08 18:07:31 +0900

    :+1: 動的にterraformリソースをclass化する仕組みを作成

--- pytest_terraform_fixture/__init__.py
+++ pytest_terraform_fixture/__init__.py
@@ -1,5 +1,6 @@
-import pytest
+import re
 
+import pytest
 from python_terraform import Terraform
 
 
@@ -35,17 +36,50 @@ def generate_terraform_fixture(target, scope='function'):
     return terraform_fixture
 
 
+def generate_terraform_function(target):
+    def terraform_function(terraform):
+        print('terraform_function: target={}'.format(target))
+        terraform.init()
+        code, out, err = terraform.apply(skip_plan=True, target=target)
+        print(code, out, err)
+        code, out, err = terraform.cmd('show')
+        print(code, out, err)
+        return out
+
+    return terraform_function
+
+
 def inject_terraform_fixture(resource, name):
+    # terraform 用に target 生成
     target = f"{resource}.{name}"
+
+    # name から動的に fixture 作成
     for scope in ['', 'function', 'class', 'module', 'session']:
         if scope == '':
             scope = 'function'
             suffix = ''
         else:
             suffix = '_{}'.format(scope)
-
         globals()[name + suffix] = generate_terraform_fixture(target, scope=scope)
+
+
+def inject_terraform_class(resource, names):
+    # staticmethod
+    staticmethods = dict()
+    for name in names:
+        # terraform 用に target 生成
+        target = f"{resource}.{name}"
+        staticmethods[name] = staticmethod(generate_terraform_function(target))
+
+    # スネークケースからキャメルケースに変更
+    clazz = re.sub("_(.)", lambda x: x.group(1).upper(), resource.capitalize())
+
+    # resource から動的に class 作成
+    globals()[clazz] = type('class', (object,), staticmethods)
 
 
 inject_terraform_fixture('nifcloud_instance', 'instance_a')
 inject_terraform_fixture('nifcloud_instance', 'instance_b')
+inject_terraform_class('nifcloud_instance', ['instance_a', 'instance_b'])
+
+print(globals())
