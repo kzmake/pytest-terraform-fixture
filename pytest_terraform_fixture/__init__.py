57db74056088e9e5e97b46de4198ad53b63e696e
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-11-08 19:38:52 +0900

    :+1: Terraformのリソースを動的クラス+メソッド化する仕組みを追加

--- pytest_terraform_fixture/__init__.py
+++ pytest_terraform_fixture/__init__.py
@@ -1,6 +1,11 @@
+import glob
+import os
 import re
+from collections import defaultdict
 
+import hcl
 import pytest
+from dotted.utils import dot
 from python_terraform import Terraform
 
 
@@ -10,10 +15,12 @@ def pytest_addoption(parser):
                      help='testing environment: active or standby')
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session', autouse=True)
 def terraform_dir(request):
     """terraform-dirオプション取得用fixture"""
     terraform_dir = request.config.getoption('--terraform-dir')
+    # TODO pytest args の取り出し方がわからなかったので暫定的に env へセット
+    os.putenv('TERRAFROM_DIR', terraform_dir)
     return terraform_dir
 
 
@@ -27,10 +34,8 @@ def generate_terraform_fixture(target, scope='function'):
     def terraform_fixture(terraform):
         print('terraform_fixture: target={}'.format(target))
         terraform.init()
-        code, out, err = terraform.apply(skip_plan=True, target=target)
-        print(code, out, err)
-        code, out, err = terraform.cmd('show')
-        print(code, out, err)
+        terraform.apply(skip_plan=True, target=target)
+        code_, out, err_ = terraform.cmd('show')
         return out
 
     return terraform_fixture
@@ -40,10 +45,8 @@ def generate_terraform_function(target):
     def terraform_function(terraform):
         print('terraform_function: target={}'.format(target))
         terraform.init()
-        code, out, err = terraform.apply(skip_plan=True, target=target)
-        print(code, out, err)
-        code, out, err = terraform.cmd('show')
-        print(code, out, err)
+        terraform.apply(skip_plan=True, target=target)
+        code_, out, err_ = terraform.cmd('show')
         return out
 
     return terraform_function
@@ -78,8 +81,34 @@ def inject_terraform_class(resource, names):
     globals()[clazz] = type('class', (object,), staticmethods)
 
 
-inject_terraform_fixture('nifcloud_instance', 'instance_a')
-inject_terraform_fixture('nifcloud_instance', 'instance_b')
-inject_terraform_class('nifcloud_instance', ['instance_a', 'instance_b'])
+def pytest_cmdline_preparse(config, args):
+    # TODO: どうにかしたい
+    dir = ''
+    for arg in args:
+        m = re.match(r'--terraform-dir=(.*)', arg)
+        if m:
+            dir = m.groups()[0]
 
-print(globals())
+    # --terraform-dir で指定されたディレクトリから *.tf を取得
+    tf_files = glob.glob('{terraform_dir}/*.tf'.format(terraform_dir=dir))
+
+    # --terraform-dir で指定されたディレクトリから resource "hogehoge" "fugafuga" {} の定義を取り出す
+    resources = defaultdict(lambda: [])
+    for tf_file in tf_files:
+        # hcl で記述されている .tf を開く
+        with open(tf_file, 'r') as f:
+            obj = dot(hcl.load(f))
+
+        # resource セクションのみ処理
+        if 'resource' in list(obj.keys()):
+            terraform_resource = obj.resource
+            for resource_key, resource in terraform_resource.items():
+                for name, param in resource.items():
+                    resources[resource_key].append(name)
+
+    for resource, names in resources.items():
+        print(resource, names)
+        for name in names:
+            inject_terraform_fixture(resource, name)
+
+        inject_terraform_class(resource, names)
