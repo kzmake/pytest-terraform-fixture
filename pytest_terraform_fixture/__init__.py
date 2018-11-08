e81c9fe6a20a1fe695ba2e7627db0cff5b164d8d
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-11-09 02:01:00 +0900

    :recycle: terraform apply結果をyamlでパース

--- pytest_terraform_fixture/__init__.py
+++ pytest_terraform_fixture/__init__.py
@@ -1,12 +1,11 @@
 import glob
-import os
-import re
-
 from collections import defaultdict
 
 import hcl
+import os
 import pytest
-
+import re
+import yaml
 from dotted.utils import dot
 from python_terraform import Terraform
 
@@ -15,72 +14,6 @@ def pytest_addoption(parser):
     """--terraform-dirの任意オプション"""
     parser.addoption('--terraform-dir', action='store', default='',
                      help='testing environment: active or standby')
-
-
-@pytest.fixture(scope='session', autouse=True)
-def terraform_dir(request):
-    """terraform-dirオプション取得用fixture"""
-    terraform_dir = request.config.getoption('--terraform-dir')
-    # TODO pytest args の取り出し方がわからなかったので暫定的に env へセット
-    os.putenv('TERRAFROM_DIR', terraform_dir)
-    return terraform_dir
-
-
-@pytest.fixture(scope='session')
-def terraform(terraform_dir):
-    return Terraform(working_dir=terraform_dir)
-
-
-def generate_terraform_fixture(target, scope='function'):
-    @pytest.fixture(scope=scope)
-    def terraform_fixture(terraform):
-        print('terraform_fixture: target={}'.format(target))
-        terraform.init()
-        terraform.apply(skip_plan=True, target=target)
-        code_, out, err_ = terraform.cmd('show')
-        return out
-
-    return terraform_fixture
-
-
-def generate_terraform_function(target):
-    def terraform_function(terraform):
-        print('terraform_function: target={}'.format(target))
-        terraform.init()
-        terraform.apply(skip_plan=True, target=target)
-        code_, out, err_ = terraform.cmd('show')
-        return out
-
-    return terraform_function
-
-
-def inject_terraform_fixture(resource, name):
-    # terraform 用に target 生成
-    target = f"{resource}.{name}"
-
-    # name から動的に fixture 作成
-    for scope in ['', 'function', 'class', 'module', 'session']:
-        if scope == '':
-            scope = 'function'
-            suffix = ''
-        else:
-            suffix = '_{}'.format(scope)
-        globals()[name + suffix] = generate_terraform_fixture(target, scope=scope)
-
-
-def inject_terraform_class(resource, names):
-    # staticmethod
-    staticmethods = dict()
-    for name in names:
-        # terraform 用に target 生成
-        target = f"{resource}.{name}"
-        staticmethods[name] = staticmethod(generate_terraform_function(target))
-
-    # スネークケースからキャメルケースに変更
-    clazz = re.sub("_(.)", lambda x: x.group(1).upper(), resource.capitalize())
-
-    # resource から動的に class 作成
-    globals()[clazz] = type('class', (object,), staticmethods)
 
 
 def pytest_cmdline_preparse(config, args):
@@ -114,3 +47,74 @@ def pytest_cmdline_preparse(config, args):
             inject_terraform_fixture(resource, name)
 
         inject_terraform_class(resource, names)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def terraform_dir(request):
+    """terraform-dirオプション取得用fixture"""
+    terraform_dir = request.config.getoption('--terraform-dir')
+    # TODO pytest args の取り出し方がわからなかったので暫定的に env へセット
+    os.putenv('TERRAFROM_DIR', terraform_dir)
+    return terraform_dir
+
+
+@pytest.fixture(scope='session')
+def terraform(terraform_dir):
+    return Terraform(working_dir=terraform_dir)
+
+
+def parser_state(state: str):
+    s = state.replace("=", ":")
+    return yaml.load(s)
+
+
+def exec_terraform(terraform, target):
+    terraform.init()
+    terraform.apply(skip_plan=True, target=target)
+    return terraform.cmd('state show {}'.format(target))
+
+
+def generate_terraform_fixture(target, scope='function'):
+    @pytest.fixture(scope=scope)
+    def terraform_fixture(terraform):
+        code_, out, err_ = exec_terraform(terraform, target)
+        return parser_state(out)
+
+    return terraform_fixture
+
+
+def generate_terraform_function(target):
+    def terraform_function(terraform):
+        code_, out, err_ = exec_terraform(terraform, target)
+        return parser_state(out)
+
+    return terraform_function
+
+
+def inject_terraform_fixture(resource, name):
+    # terraform 用に target 生成
+    target = f"{resource}.{name}"
+
+    # name から動的に fixture 作成
+    for scope in ['', 'function', 'class', 'module', 'session']:
+        if scope == '':
+            scope = 'function'
+            suffix = ''
+        else:
+            suffix = '_{}'.format(scope)
+        globals()[name + suffix] = generate_terraform_fixture(target, scope=scope)
+
+
+def inject_terraform_class(resource, names):
+    # staticmethod
+    staticmethods = dict()
+    for name in names:
+        # terraform 用に target 生成
+        target = f"{resource}.{name}"
+        staticmethods[name] = staticmethod(generate_terraform_function(target))
+
+    # スネークケースからキャメルケースに変更
+    clazz = re.sub("_(.)", lambda x: x.group(1).upper(), resource.capitalize())
+
+    # resource から動的に class 作成
+    globals()[clazz] = type('class', (object,), staticmethods)
