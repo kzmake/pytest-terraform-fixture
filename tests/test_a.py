6ec84a37a8fb169defefdd1dfbb8e53d0439eafa
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-11-09 02:02:43 +0900

    :recycle: 動的生成のfixtureすべてのテストを追加

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,11 +1,9 @@
 import os
-
 import pytest
-
+import re
 from python_terraform import Terraform
 
 from pytest_terraform_fixture import NifcloudInstance
-
 
 ACCESS_KEY = os.environ.get("ACCESS_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -25,15 +23,40 @@ def terraform(terraform_dir):
 
 
 @pytest.fixture(scope='function')
-def instance_a_modfixutre(terraform):
+def mod_instance_a(terraform):
     return NifcloudInstance.instance_a(terraform)
 
 
-def test_terraform_fixture(terraform_dir, instance_a):
-    print(terraform_dir, instance_a)
-    assert True
+def test_terraform_fixture_instance_a(instance_a):
+    """Fixture instance_a"""
+    assert re.match(r'i-[0-9a-z]{8}', instance_a['id'])
 
 
-def test_terraform_function(terraform_dir, instance_a_modfixutre):
-    print(terraform_dir, instance_a_modfixutre)
-    assert True
+def test_terraform_fixture_instance_b(instance_b):
+    """Fixture instance_b"""
+    assert re.match(r'i-[0-9a-z]{8}', instance_b['id'])
+
+
+def test_terraform_fixture_instance_a_scope_function(instance_a_function):
+    """Fixture instance_a scope=function"""
+    assert re.match(r'i-[0-9a-z]{8}', instance_a_function['id'])
+
+
+def test_terraform_fixture_instance_a_scope_class(instance_a_class):
+    """Fixture instance_a scope=class"""
+    assert re.match(r'i-[0-9a-z]{8}', instance_a_class['id'])
+
+
+def test_terraform_fixture_instance_a_scope_module(instance_a_module):
+    """Fixture instance_a scope=module"""
+    assert re.match(r'i-[0-9a-z]{8}', instance_a_module['id'])
+
+
+def test_terraform_fixture_instance_a_scope_session(instance_a_session):
+    """Fixture instance_a scope=module"""
+    assert re.match(r'i-[0-9a-z]{8}', instance_a_session['id'])
+
+
+def test_terraform_class_staticmethod(terraform):
+    """Class.staticmethod"""
+    assert re.match(r'i-[0-9a-z]{8}', NifcloudInstance.instance_a(terraform)['id'])
